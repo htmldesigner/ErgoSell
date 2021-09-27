6292e7654cce9c7add149e0e6e3ca60cf8649f11
htmldesigner <frontendmaster@yandex.ru>
2021-09-27 22:44:11 +0500

    added some fix

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,16 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', () => {
     const btnMnu = document.querySelector('.btnMnu')
+    const mobileMenu = document.querySelector('.mobileMenu')
+    const closeMobileMenu = document.querySelector('.closeMobileMenu')
     btnMnu.addEventListener('click', (event) => {
         btnMnu.classList.toggle('active')
+        mobileMenu.classList.toggle('active')
     })
+
+    closeMobileMenu.addEventListener('click', (event) => {
+        btnMnu.classList.remove('active')
+        mobileMenu.classList.remove('active')
+    })
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
+		// 'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
